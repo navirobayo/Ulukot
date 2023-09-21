908249da5002827eb954189389dcb66ca5da8a8f
Ivan <101908819+navirobayo@users.noreply.github.com>
2023-09-21 01:41:33 -0500

    C++ Test

---
+++ Source/ULUKOT/UlukotSpider.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UlukotSpider.h"
+
+// Sets default values
+AUlukotSpider::AUlukotSpider()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AUlukotSpider::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AUlukotSpider::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AUlukotSpider::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+// Test of the environment
+
